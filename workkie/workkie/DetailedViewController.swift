53fdfe2e5288e84a2071cc42988e85e290a5b851
paper-n-pen <b.sangwon@wustl.edu>
2024-11-11 11:54:37 -0600

    restore point

--- workkie/workkie/DetailedViewController.swift
+++ workkie/workkie/DetailedViewController.swift
@@ -8,11 +8,27 @@
 import UIKit
 
 class DetailedViewController: UIViewController {
+    
+    var image: UIImage?
+    var imageName: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let theImageFrame = CGRect(x: view.frame.midX - (image?.size.width ?? 0)/2, y: 120, width: image?.size.width ?? 0, height: image?.size.height ?? 0)
+        
+        let imageView = UIImageView(frame: theImageFrame)
+        imageView.image = image
+        
+        view.addSubview(imageView)
+        
+        let theTextFrame = CGRect(x: 0, y: image?.size.height ?? 0 + 120, width: view.frame.width, height: 30)
+        
+        let textView = UILabel(frame: theTextFrame)
+        textView.text = imageName
+        textView.textAlignment = .center
+        view.addSubview(textView)
     }
     
 
--- workkie/workkie/ViewController.swift
+++ workkie/workkie/ViewController.swift
@@ -8,12 +8,62 @@
 import UIKit
 import BSON
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return theData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        cell.textLabel!.text = theData[indexPath.row].name
+        cell.imageView?.image = theImageCache[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let detailedVC = DetailedViewController()
+        detailedVC.image = theImageCache[indexPath.row]
+        detailedVC.imageName = theData[indexPath.row].name
+        
+        navigationController?.pushViewController(detailedVC, animated: true)
+    }
+    
+    func setupTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    func fetchDataForTableView() {
+        let url = URL(string: "mongodb+srv://chengli:Luncy1234567890@users.at6lb.mongodb.net/users?authSource=admin&appName=Users")
+        let data = try! Data(contentsOf: url!)
+        theData = try! JSONDecoder().decode([Info].self, from:data)
+    }
+    
+    func cacheImages() {
+        for item in theData {
+            let url = URL(string: item.image_url)
+            let data = try? Data(contentsOf: url!)
+            let image = UIImage(data: data!)
+            theImageCache.append(image!)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        setupTableView()
+        fetchDataForTableView()
+        cacheImages()
+        
     }
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
 }
 
