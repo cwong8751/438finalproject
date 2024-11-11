419e76b7b5fe057f8d416b3998ff1188c048d8ea
paper-n-pen <b.sangwon@wustl.edu>
2024-11-11 11:51:45 -0600

    restore point

---
+++ workkie/workkie/DetailedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailedViewController.swift
+//  workkie
+//
+//  Created by Sang Won Bae on 11/10/24.
+//
+
+import UIKit
+
+class DetailedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
