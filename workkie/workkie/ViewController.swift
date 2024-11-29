3b3bc64c8eb732ac1ab9ee85e2bb6079f14909f4
paper-n-pen <brokenrectangle@gmail.com>
2024-11-29 17:22:31 -0600

    Newest posts are at the top.

--- workkie/workkie/ViewController.swift
+++ workkie/workkie/ViewController.swift
@@ -83,7 +83,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                 try await dbManager.connect(uri: url)
                 let listOfPosts = try await dbManager.getAllPosts() ?? []
                 DispatchQueue.main.async {
-                    self.tableData = listOfPosts
+                    self.tableData = listOfPosts.reversed()
                     self.tableView.reloadData()
                     self.refreshControl.endRefreshing()
                 }
